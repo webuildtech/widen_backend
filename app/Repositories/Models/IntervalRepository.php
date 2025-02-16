7f09499a5b39c93bb7f67dde7c209babfa96af5e
Patryk Prževalski <przevalskip@gmail.com>
2025-02-16 12:10:08 +0200

    Update

--- app/Repositories/Models/IntervalRepository.php
+++ app/Repositories/Models/IntervalRepository.php
@@ -6,6 +6,7 @@ use App\Interfaces\Repositories\Models\IntervalRepositoryInterface;
 use App\Models\Interval;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Spatie\LaravelData\Data;
 
 class IntervalRepository extends BaseRepository implements IntervalRepositoryInterface
@@ -34,7 +35,13 @@ class IntervalRepository extends BaseRepository implements IntervalRepositoryInt
         $values = $this->getData($data, ['prices']);
 
         isset($values['date_from']) && $values['date_from']->startOfDay();
-        isset($values['date_to']) && $values['date_to']->endOfDay();
+        if (isset($values['date_to'])) {
+            $values['date_to']->endOfDay();
+
+            if ($values['date_to'] < now()) {
+                DB::table('court_interval')->where('interval_id', $model->id)->delete();
+            }
+        }
 
         $model->update($values);
 
